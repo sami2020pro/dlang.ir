86703c37cff847c6be36d3b7288387b576dff021
miladimos <message.tome1331@gmail.com>
2021-02-01 19:29:29 +0330

    add multilangual

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: 'Vuepress Docs Boilerplate',
+  title: 'D Lang',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
@@ -16,10 +16,23 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
   head: [
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'theme-color', content: '#B03931' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
   ],
+
+  locales: {
+    '/': {
+      lang: 'en-US', // this will be set as the lang attribute on <html>
+      title: 'English',
+      description: 'English'
+    },
+    '/fa/': {
+      lang: 'fa-IR',
+      title: 'فارسی',
+      description: 'فارسی'
+    }
+  },
 
   /**
    * Theme configuration, here is the default theme configuration for VuePress.
@@ -27,36 +40,81 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
    */
   themeConfig: {
-    repo: '',
-    editLinks: false,
-    docsDir: '',
-    editLinkText: '',
-    lastUpdated: false,
-    nav: [
-      {
-        text: 'Guide',
-        link: '/guide/',
-      },
-      {
-        text: 'Config',
-        link: '/config/'
-      },
-      {
-        text: 'VuePress',
-        link: 'https://v1.vuepress.vuejs.org'
-      }
-    ],
-    sidebar: {
-      '/guide/': [
-        {
-          title: 'Guide',
-          collapsable: false,
-          children: [
-            '',
-            'using-vue',
-          ]
+    locales:{
+      '/' : {
+        repo: '',
+        editLinks: false,
+        docsDir: '',
+        editLinkText: '',
+        lastUpdated: false,
+        nav: [
+          {
+            text: 'Guide',
+            link: '/guide/',
+          },
+          {
+            text: 'Library',
+            link: '/config/'
+          },
+          {
+            text: 'DLang',
+            link: 'https://dlang.org/'
+          }
+        ],
+        sidebar: {
+          '/guide/': [
+            {
+              title: 'Guide',
+              collapsable: false,
+              children: [
+                '',
+                'using-vue',
+              ]
+            }
+          ],
         }
-      ],
+      },
+      '/fa/' : {
+        repo: '',
+        editLinks: false,
+        docsDir: '',
+        editLinkText: '',
+        lastUpdated: false,
+        nav: [
+          {
+            text: 'Guide',
+            link: '/guide/',
+          },
+          {
+            text: 'Library',
+            link: '/config/'
+          },
+          {
+            text: 'DLang',
+            link: 'https://dlang.org/'
+          }
+        ],
+        sidebar: {
+          '/guide/': [
+            {
+              title: 'Guide',
+              collapsable: false,
+              children: [
+                '',
+                'using-vue',
+              ]
+            }
+          ],
+        }
+      }
+    }
+  },
+
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@alias': '../public'
+      }
     }
   },
 
